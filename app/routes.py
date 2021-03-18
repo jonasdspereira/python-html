5e1a9c4fdca00f799e007fcd5343e0d56ddeb09a
Jonas <jnsprr@outlook.com>
2021-03-18 23:02:35 +0000

    login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import current_user, login_user
+from app.models import User
 
 
 @app.route('/')
@@ -7,7 +9,22 @@ from flask import render_template
 def index():
     user = { 'username': 'Jonas'}
     posts = [
-        {'author': {'username': 'Jonas'}, 'body': "Oi mozinho"},
-        {'author': {'username': 'Eduardo'}, 'body': "Oi meu bb <3"}
+        {'author': {'username': 'Jonas'}, 'body': "Oi!"},
+        {'author': {'username': 'Eduardo'}, 'body': "Olá!"}
     ]
-    return render_template("index.html", user=user, posts=posts, title="teste")
+    return render_template("index.html", user=user, posts=posts)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+     if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template("login.html", title="Login")
